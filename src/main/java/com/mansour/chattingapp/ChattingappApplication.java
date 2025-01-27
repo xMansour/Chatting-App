058f1b626493e1bfbaf6a1a6e6541966180c2e8e
xMansour <memansour96@gmail.com>
2025-01-27 20:11:26 +0200

    App init

---
+++ src/main/java/com/mansour/chattingapp/ChattingappApplication.java
@@ -0,0 +1,13 @@
+package com.mansour.chattingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattingappApplication.class, args);
+	}
+
+}
