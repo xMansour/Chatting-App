b16760d76e803808b58d4e51d10726fb8fe03fce
xMansour <memansour96@gmail.com>
2025-01-27 21:04:35 +0200

    Auditing basics

---
+++ src/main/java/com/mansour/chattingapp/audit/config/JpaAuditingConfig.java
@@ -0,0 +1,17 @@
+package com.mansour.chattingapp.audit.config;
+
+import com.mansour.chattingapp.audit.listener.AuditorAwareImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+public class JpaAuditingConfig {
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return new AuditorAwareImpl();
+    }
+}
